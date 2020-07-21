19edcf2e1fafb36492f09b099194aadd2d1ea521
bshin95 <shinbryann@gmail.com>
2020-07-21 16:44:46 -0400

    changes

---
+++ app/client/src/components/UserComponents/Purchase.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react"
+
+export default class Purchase extends Component {
+  render() {
+    return <div></div>
+  }
+}
---
+++ app/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://bshin95:bshin120895@cluster0.qtmrr.mongodb.net/<dbname>?retryWrites=true&w=majority",
+  secretOrKey: "secret",
+}
