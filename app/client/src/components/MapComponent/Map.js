f71b3d04cd98c9fac8b02c23d094389c55822aa8
bshin95 <shinbryann@gmail.com>
2020-07-24 13:25:31 -0400

    venue map

---
+++ app/client/src/components/MapComponent/Map.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react"
+
+class Map extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>Venue Map</h1>
+        </div>
+        <div>
+          <img src="https://blog.ticketiq.com/hs-fs/hubfs/BarclaysCenter_BrunoMars_2017-10-04_2016-11-16_1027_SVGC_tn.gif?width=525&name=BarclaysCenter_BrunoMars_2017-10-04_2016-11-16_1027_SVGC_tn.gif"></img>
+        </div>
+        <div>
+          <p>Entrance - Exits</p>
+          <p>Restrooms</p>
+          <p>Sanitizing Sanitation</p>
+          <p>Food & Drinks</p>
+          <p>Guest Services</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Map
