0bf830c46a0eb71ede8859878c173da8b87d968a
bshin95 <shinbryann@gmail.com>
2020-07-24 12:13:26 -0400

    css changes

--- app/client/src/components/Profile/Description.js
+++ app/client/src/components/Profile/Description.js
@@ -1,22 +1,48 @@
 import React, { Component } from "react"
+import styled from "styled-components"
+import LogoComponent from "../Logo/LogoComponent"
+import { Link } from "react-router-dom"
+
+const ProfileContainer = styled.div``
+
+const Name = styled.h1`
+  font-size: 5rem;
+`
+
+const ProfileDescription = styled.div`
+  font-size: 2rem;
+  padding: 0;
+`
+
+const PicContainer = styled.div`
+  text-align: center;
+  overflow: hidden;
+  /* border-radius: 50%; */
+`
+const ProPic = styled.img`
+  width: 70%;
+  border-radius: 50%;
+`
 
 class Description extends Component {
   render() {
     const { age, name, location } = this.props
     return (
-      <div>
+      <ProfileContainer>
+        <Link to="/dashboard">
+          <LogoComponent />
+        </Link>
+        <PicContainer>
+          <ProPic src="https://images.unsplash.com/photo-1543486958-d783bfbf7f8e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80"></ProPic>
+        </PicContainer>
         <div>
-          <h1>{name}</h1>
+          <Name>{name}</Name>
         </div>
-        <div>
-          <div>
-            <p>Age: {age}</p>
-          </div>
-          <div>
-            <p>{location}</p>
-          </div>
-        </div>
-      </div>
+        <ProfileDescription>
+          <p>Age: {age}</p>
+          <p>{location}</p>
+        </ProfileDescription>
+      </ProfileContainer>
     )
   }
 }
